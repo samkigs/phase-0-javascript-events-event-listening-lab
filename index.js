05e24271be19c812591697d9b4c8d1b7fbefdfa8
samkigs <samuel.kihuga@student.moringaschool.com>
2022-06-10 09:59:35 +0300

    EVENT LISTENING

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 function addingEventListener() {
-}
+
+    const input = document.getElementById('input');
+        
+    function clickAlert() {
+      alert('Yessir Yessir!');
+    }
+    
+    input.addEventListener('click', clickAlert());
+    
+        
+    }
